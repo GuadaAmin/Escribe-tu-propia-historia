48affb9eb8d8a429dcb15a503a9b97632a6dca9c
guadaamin <guaddamin@gmail.com>
2022-05-29 20:47:15 -0300

    escribe tu historia final

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let historia = "Había " ;
+let input = 0;
+let continuar = 0;
+
+alert("Escribamos una historia :]");
+continuar = prompt("Escriba ESC si no desea continuar o enter para escribir una historia:");
+while (continuar != "ESC") {
+    alert(historia);
+    input = prompt("Ingrese la siguiente palabra:");
+    historia = historia + input;
+    switch (input) {
+        case "una":
+            historia = historia + " vez ";
+            break;
+        case "un":
+            historia = historia + " gato ";
+            break;
+        case "que":
+            historia = historia + " dormía.";
+            break;
+        case "muy":
+            historia = historia + " lindo.";
+            break;
+        default:
+            historia = historia + " perros ";
+            break;
+    }
+    if (input == "que" || input == "muy") {
+        alert(historia);
+        break;
+    }
+    if (input == "") {
+        alert("Así no se puede escribir una historia :(");
+        break;
+    }
+}
+if (continuar == "ESC") {
+    alert("No hay problema, ¡nos vemos la próxima!")
+} else if (input == "") {
+    alert("¡Recargá la página e intentá de nuevo!")
+} else {
+    alert("¡Gracias por escribir una historia conmigo!")
+}
+
+
